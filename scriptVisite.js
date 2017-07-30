a61717b3beca6c69c1e685580157060d54ab5e90
Cracheur <cracheurderat@gmail.com>
2017-07-30 22:47:29 +0200

    update

--- scriptVisite.js
+++ scriptVisite.js
@@ -19,26 +19,54 @@ ProfileLinkGetter.prototype = {
 function ProfileVisitorEngine(profileLinkGetter){
     this.linksToVisit = profileLinkGetter.getLinks();
     this.requestedLink = [];
+    this.visitedLinks = []
+    localStorage.setItem('visited',JSON.stringify([]));
 }
 
 ProfileVisitorEngine.prototype = {
     visitProfiles: function (){
+        this.requestedLink = this.getVisitableProfiles();
+        console.log("ICI = " + this.requestedLink);
         this.requestedLink = this.linksToVisit
                                  .slice()
                                  .map($.ajax);
-        var getVisited = this.getVisitedLinks().bind(this);
+        var getVisited = this.getVisitedLinks.bind(this);
         $.when.apply($, this.requestedLink)
-              .then(getVisited,getVisited);
+              .then(getVisited, getVisited)
+              .always(this.setVisitedProfilesInLocalStorage);
     },
 
     getVisitedLinks: function(request){
-        console.log(this.requestedLink.status());
+        this.visitedLinks = this.linksToVisit.slice();
+        this.requestedLink.forEach(function(request, index){
+            if(request.status != 200)
+                this.visitedLinks.splice(index, 1);
+        });
+        return this.visitedLinks;
+    },
+
+    setVisitedProfilesInLocalStorage: function(visitedProfiles){
+        var visitedProfilesLS = JSON.parse(localStorage.getItem("visited")) || [];
+        console.log("POPO = " + visitedProfiles);
+        console.log("PIPI = " + visitedProfilesLS);
+        console.log("PEPE = " + localStorage.getItem("visited"));
+        console.log("PAPA = " + JSON.stringify(visitedProfilesLS.concat(visitedProfiles)));
+        localStorage.setItem('visited',
+                         JSON.stringify(visitedProfiles.concat(visitedProfilesLS)));
+    },
+
+    getVisitableProfiles: function(){
+        var visitedProfiles = JSON.parse(localStorage.getItem("visited")) || [];
+        console.log(visitedProfiles);
+        return this.linksToVisit
+                   .filter(function(link){
+                       return !visitedProfiles.includes(link)
+                   });
     }
 }
 
 var visitor = new ProfileVisitorEngine(new ProfileLinkGetter(document.body));
 visitor.visitProfiles();
-
 
 function visitNewProfiles(){
     $.ajax(window.location.href)
